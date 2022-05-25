c8b6c52e435955629b5dac0f563a381d0ff0d85a
Homutok <81097991+Homutok@users.noreply.github.com>
2022-05-25 16:45:55 +0300

    ADD JWT_AUTH
    Добавлена авторизация по JWT токенам

--- backend/urls.py
+++ backend/urls.py
@@ -19,6 +19,7 @@ from rest_framework import routers
 from Purpose.views import PurposeViewSet
 from Blog.views import PostViewSet, FoodViewSet
 from Person.views import PersonViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 router = routers.DefaultRouter()
 router.register(r'Blog', PostViewSet)
@@ -30,4 +31,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
